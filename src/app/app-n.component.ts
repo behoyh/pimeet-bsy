03802c2b609e597bc12e343550c5ce44c62b616c
Beshoy Hanna <beshoyhanna@outlook.com>
2023-05-14 17:55:07 -0700

    Add files via upload

---
+++ src/app/app-n.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+
+import ZoomMtgEmbedded from '@zoomus/websdk/embedded';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  authEndpoint = 'https://pizookie.herokuapp.com/'
+  sdkKey = 'Uaty1iKCQAyoJElAMLZhRQ'
+  meetingNumber = '123456789'
+  passWord = ''
+  role = 0
+  userName = 'Angular'
+  userEmail = ''
+  registrantToken = ''
+  zakToken = ''
+
+  client = ZoomMtgEmbedded.createClient();
+
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
+
+  }
+
+  ngOnInit() {
+    let meetingSDKElement = document.getElementById('meetingSDKElement');
+
+    this.client.init({
+      debug: true,
+      zoomAppRoot: meetingSDKElement,
+      language: 'en-US',
+      customize: {
+        meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+        toolbar: {
+          buttons: [
+            {
+              text: 'Custom Button',
+              className: 'CustomButton',
+              onClick: () => {
+                console.log('custom button');
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+
+  getSignature() {
+    this.httpClient.post(this.authEndpoint, {
+	    meetingNumber: this.meetingNumber,
+	    role: this.role
+    }).toPromise().then((data: any) => {
+      if(data.signature) {
+        console.log(data.signature)
+        this.startMeeting(data.signature)
+      } else {
+        console.log(data)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  startMeeting(signature) {
+
+    this.client.join({
+      signature: signature,
+    	sdkKey: this.sdkKey,
+    	meetingNumber: this.meetingNumber,
+    	password: this.passWord,
+    	userName: this.userName,
+      userEmail: this.userEmail,
+      tk: this.registrantToken,
+      zak: this.zakToken
+    })
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+
+import { ZoomMtg } from '@zoomus/websdk';
+
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.12.0/lib', '/av');
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load('en-US');
+ZoomMtg.i18n.reload('en-US');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  authEndpoint = 'https://pizookie.herokuapp.com/'
+  sdkKey = 'Uaty1iKCQAyoJElAMLZhRQ'
+  meetingNumber = '123456789'
+  passWord = ''
+  role = 0
+  userName = 'Angular'
+  userEmail = ''
+  registrantToken = ''
+  zakToken = ''
+  leaveUrl = 'http://localhost:4200'
+
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  getSignature() {
+    this.httpClient.post(this.authEndpoint, {
+	    meetingNumber: this.meetingNumber,
+	    role: this.role
+    }).toPromise().then((data: any) => {
+      if(data.signature) {
+        console.log(data.signature)
+        this.startMeeting(data.signature)
+      } else {
+        console.log(data)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  startMeeting(signature) {
+
+    document.getElementById('zmmtg-root').style.display = 'block'
+
+    ZoomMtg.init({
+      leaveUrl: this.leaveUrl,
+      success: (success) => {
+        console.log(success)
+        ZoomMtg.join({
+          signature: signature,
+          sdkKey: this.sdkKey,
+          meetingNumber: this.meetingNumber,
+          passWord: this.passWord,
+          userName: this.userName,
+          userEmail: this.userEmail,
+          tk: this.registrantToken,
+          zak: this.zakToken,
+          success: (success) => {
+            console.log(success)
+          },
+          error: (error) => {
+            console.log(error)
+          }
+        })
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+}
